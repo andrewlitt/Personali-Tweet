0b13e7ab9954a5d0280878344695c37f362548d0
andrewlitt <andrew.litt@queensu.ca>
2017-01-22 10:31:35 -0500

    YEEEEEEEEAH BOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOY

--- app/src/containers/GraphContainer.js
+++ app/src/containers/GraphContainer.js
@@ -10,10 +10,11 @@ const GraphContainer = React.createClass({
     render () {
         return Object.keys(this.props.Data).length > 0 ? (
             <div className="graphContainer">
-                <RadarGraph data={this.props.Data.graphs.taxData} />
+								<h1>An Analysis of Tweets From @{this.props.Data.username}</h1>
+								<BarGraph data={this.props.Data.graphs.sentimentData}/>
                 <PieGraph data={this.props.Data.graphs.emotionData}/>
                 <RadialGraph data={this.props.Data.graphs.conceptData}/>
-                <BarGraph data={this.props.Data.graphs.sentimentData}/>
+                <RadarGraph data={this.props.Data.graphs.taxData} />
                 <CloudGraph data={this.props.Data.graphs.keywordData}/>
 								<FakeTweet picture={this.props.Data.picture} name={this.props.Data.name} username={this.props.Data.username} tweet={this.props.Data.tweet} />
             </div>
