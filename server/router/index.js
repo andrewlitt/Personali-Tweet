d169030ddde0376198e4c4ddba9c4acfb553a6dc
zharley18 <zackharley@gmail.com>
2017-01-22 02:50:35 -0500

    Fix issue with requires

--- server/router/index.js
+++ server/router/index.js
@@ -3,9 +3,7 @@ const cors = require('./../controllers/cors');
 const tweets = require('./../controllers/tweets');
 const markovChain = require('./../controllers/markovChain');
 const user = require('./../controllers/user');
-const tweets = require('./../controllers/tweets');
 const search = require('./../controllers/search');
-const markovChain = require('./../controllers/markovChain');
 router.use(cors);
 
 router.get('/api/user/:user', tweets)
