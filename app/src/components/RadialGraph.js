afc0fb6f89352ec24028e44896114fa01d2d09eb
andrewlitt <andrew.litt@queensu.ca>
2017-01-22 01:24:29 -0500

    wtf theres more

--- app/src/components/RadialGraph.js
+++ app/src/components/RadialGraph.js
@@ -3,13 +3,13 @@ const {RadialBarChart, RadialBar, Legend} = require('recharts');
     
   const style = {
   	top: 0,
-  	left: 350,
+  	left: "75%",
   	lineHeight: '30px'
   };
 var RadialGraph = React.createClass({
 	render () {
   	return (
-    	<RadialBarChart width={700} height={300} cx={150} cy={150} innerRadius={20} outerRadius={140} barSize={10} data={this.props.data}>
+    	<RadialBarChart width={700} height={290} cx="50%" cy="50%" innerRadius={20} outerRadius={140} barSize={10} data={this.props.data}>
 		        <RadialBar minAngle={15} label background clockWise={true} dataKey='relevance'/>
 		        <Legend iconSize={10} width={120} height={140} layout='vertical' verticalAlign='middle' wrapperStyle={style}/>
         </RadialBarChart>
